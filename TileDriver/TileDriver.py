beeeb166019c76cdc6d3f1f9c9232ace1cfd68fe
lukesmith96 <lsmith41@calpoly.edu>
2017-09-21 10:33:02 -0700

    Add Manhattan Distance and most of shuffle all dynamic

--- TileDriver/TileDriver.py
+++ TileDriver/TileDriver.py
@@ -1,5 +1,4 @@
 from random import randint
-import numpy as np
 
 #
 # Dir [0:UP 1:DOWN 2:LEFT 3:RIGHT]
@@ -7,52 +6,56 @@ import numpy as np
 #
 
 def main():
-	width = int(input("Enter Grid Width: "))
-	shuffleTiles(width)
+   width = int(input("Enter Grid Width: "))
+   shuffleTiles(width)
 
 def shuffleTiles(width):
-    grid = [[0 for j in range(width)] for i in range(width)]
-    vals = list(range(0, (width * width)))
-    empx = 0
-    empy = 0
-    for i in range(0, width):
-        for j in range(0, width):
-            randInt = randint(0, len(vals) - 1)
-            val = vals[randInt]
-            if (val == 0):
-                empx = i
-                empy = j
-            del vals[randInt]
-            grid[i][j] = val
-	#check if the grid is valid
-    isValidMove(grid, empx, empy, dir)
-    printGrid(grid)
-    return grid
+   grid = [[0 for j in range(width)] for i in range(width)]
+   vals = list(range(0, (width * width)))
+   empx = 0
+   empy = 0
+   for i in range(0, width):
+      for j in range(0, width):
+         randInt = randint(0, len(vals) - 1)
+         val = vals[randInt]
+         if (val == 0):
+            empx = i
+            empy = j
+         del vals[randInt]
+         grid[i][j] = val
+   #check if the grid is valid
+   isValidMove(grid, empx, empy, dir)
+   dist = getManhattanDist(grid)
+   printGrid(grid)
+   return grid
 
-def getManhattanDist(grid, x, y):
-    dictX = list(range(0, len(grid) * len(grid)))
-    finish = np.array(list).reshape(len(grid), len(grid))
-    dist = 0;
-    for i in range(0, len(grid)):
-        for j in range(0, len(grid)):
-            copy = copy(grid)
-            if (grid[i][j] == 1):
-                dist += i + (j - 1)
+def getManhattanDist(grid):
+   width = len(grid)
+   dist = 0;
+   for i in range(0, len(grid)):
+      for j in range(0, len(grid)):
+         var = grid[i][j]
+         finX = var % width
+         finY = int(var/width)
+         dist += abs(i - finX) + abs(j - finY)
+   return dist
 
 def copy(grid):
-    return grid
+   return grid
+
 def isValidMove(grid, x, y, dir):
-    if ((dir == 0 and grid[x - 1, y] is not None) 
-        or (dir == 1 and grid[x + 1, y] is not None) 
-        or (dir == 2 and grid[x, y - 1] is not None) 
-        or (dir == 3 and grid[x, y + 1] is not None)):
-       return True
-    return False
+   if ((dir == 0 and grid[x - 1, y] is not None) 
+    or (dir == 1 and grid[x + 1, y] is not None) 
+    or (dir == 2 and grid[x, y - 1] is not None) 
+    or (dir == 3 and grid[x, y + 1] is not None)):
+      return True
+   return False
+
 def printGrid(grid):
-	for list in grid:
-		for number in list:
-			print(number, ",", end="")
-		print("\n")
+   for list in grid:
+      for number in list:
+         print(number, ",", end="")
+      print("\n")
 
 if __name__ == '__main__':
-    main()
+   main()
