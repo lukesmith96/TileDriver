64e1fefe3818dbf1393db597aab3c9ebe928279f
lukesmith96 <lsmith41@calpoly.edu>
2017-09-21 00:24:28 -0700

    Add game init

--- TileDriver/TileDriver.py
+++ TileDriver/TileDriver.py
@@ -1,27 +1,53 @@
 from random import randint
+import numpy as np
+
+#
+# Dir [0:UP 1:DOWN 2:LEFT 3:RIGHT]
+# 
+#
 
 def main():
 	width = int(input("Enter Grid Width: "))
 	shuffleTiles(width)
 
 def shuffleTiles(width):
-	grid = createPossibleGrid(width)
+    grid = [[0 for j in range(width)] for i in range(width)]
+    vals = list(range(0, (width * width)))
+    empx = 0
+    empy = 0
+    for i in range(0, width):
+        for j in range(0, width):
+            randInt = randint(0, len(vals) - 1)
+            val = vals[randInt]
+            if (val == 0):
+                empx = i
+                empy = j
+            del vals[randInt]
+            grid[i][j] = val
 	#check if the grid is valid
-	printGrid(grid)
-	return grid
+    isValidMove(grid, empx, empy, dir)
+    printGrid(grid)
+    return grid
 
-def createPossibleGrid(width):
-	grid = [[0 for j in range(width)] for i in range(width)]
-	vals = list(range(0, (width * width)))
+def getManhattanDist(grid, x, y):
+    dictX = list(range(0, len(grid) * len(grid)))
+    finish = np.array(list).reshape(len(grid), len(grid))
+    dist = 0;
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid)):
+            copy = copy(grid)
+            if (grid[i][j] == 1):
+                dist += i + (j - 1)
 
-	for i in range(0, width):
-		for j in range(0, width):
-			randInt = randint(0, len(vals) - 1)
-			val = vals[randInt]
-			del vals[randInt]
-			grid[i][j] = val
-	return grid
-
+def copy(grid):
+    return grid
+def isValidMove(grid, x, y, dir):
+    if ((dir == 0 and grid[x - 1, y] is not None) 
+        or (dir == 1 and grid[x + 1, y] is not None) 
+        or (dir == 2 and grid[x, y - 1] is not None) 
+        or (dir == 3 and grid[x, y + 1] is not None)):
+       return True
+    return False
 def printGrid(grid):
 	for list in grid:
 		for number in list:
